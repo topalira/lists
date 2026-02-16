0733ce5f3b650a24e735f2cbc8f0e9a9864c4107
topar82 <topar82@29-5>
2026-02-16 10:18:31 +0200

    111

--- src/components/Mylist/Mylist.js
+++ src/components/Mylist/Mylist.js
@@ -1,20 +1,20 @@
 import './Mylist.css';
 
 const products = [
-  { title: 'Капуста', isFruit: false, id: 1 },
-  { title: 'Чеснок', isFruit: false, id: 2 },
-  { title: 'Яблоко', isFruit: true, id: 3 },
-  { title: 'Киви', isFruit: true, id: 4 },
-  { title: 'Лук', isFruit: false, id: 5 },
-  { title: 'Картошка', isFruit: false, id: 6 },
-  { title: 'Бананы', isFruit: true, id: 7 },
-  { title: 'Ананасы', isFruit: true, id: 8 }
+  { title: 'Капуста', isFruit: false, id: 1, count: "5kg" },
+  { title: 'Чеснок', isFruit: false, id: 2, count: "0.5kg" },
+  { title: 'Яблоко', isFruit: true, id: 3, count: "5kg" },
+  { title: 'Киви', isFruit: true, id: 4, count: "2kg" },
+  { title: 'Лук', isFruit: false, id: 5, count: "3kg" },
+  { title: 'Картошка', isFruit: false, id: 6, count: "6kg" },
+  { title: 'Бананы', isFruit: true, id: 7, count: "1kg" },
+  { title: 'Ананасы', isFruit: true, id: 8, count: "2kg" }
 ]
 
 function Elem(props) {
   const style = { color: props.item.isFruit ? "magenta" : "green" }
   return <li style={style}>
-    {props.item.title} - {props.item.id}
+    {props.item.title} - {props.item.count}
   </li>
 }
 
